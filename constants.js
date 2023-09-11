ba963d097f9101b6ab7b0693c327ae8327450cd8
AjayReddy19 <143324875+AjayReddy19@users.noreply.github.com>
2023-09-11 19:35:57 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,42 @@
+// Options the user could type in
+const prompts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    [
+        "why should i use this website",
+        "Benefit of using this website",
+        "Why should I prefer this website"
+    ],
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!"],
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    [
+        "as it contains all features of a travelling and tourism website we can use this",
+    ]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Try Again...",
+    "I don't understand :/"
+  ]
+  
+  // Whatever else you want :)
+  
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
